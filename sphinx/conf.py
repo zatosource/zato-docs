8e750692cd42432b74b9ac51e6c23600d83cde7a
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-11-06 18:29:20 +0100

    Made make html work again.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -62,7 +62,7 @@ today_fmt = '%B %d, %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'zato_docs_env']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
